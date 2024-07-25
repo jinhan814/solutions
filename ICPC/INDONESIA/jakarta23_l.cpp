3724d6b5c98f3decb162a14fb05c053f3648f4d3
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2024-07-26 03:18:15 +0900

    .

---
+++ ICPC/INDONESIA/jakarta23_l.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+
+int main() {
+	fastio;
+	int n, m; cin >> n >> m;
+	if (m < n / 2 || m == n) return cout << -1 << '\n', 0;
+	int d = m - n / 2;
+	string res;
+	for (int i = 0; i < d >> 1; i++) res += "()";
+	for (int i = 0; i < n / 2 - d; i++) res += "(";
+	for (int i = 0; i < n / 2 - d; i++) res += ")";
+	for (int i = 0; i < d + 1 >> 1; i++) res += "()";
+	cout << res << '\n';
+}

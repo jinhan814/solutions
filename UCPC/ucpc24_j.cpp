219eaa8cb5ddc321c877723cdfd10a8b9744506b
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2024-08-06 21:28:30 +0900

    .

--- UCPC/ucpc24_j.cpp
+++ UCPC/ucpc24_j.cpp
@@ -12,7 +12,7 @@ int main() {
 		adj[b].push_back(a);
 	}
 
-	auto get_dist = [&](int x) {
+	auto calc = [&](int x) {
 		vector d(n + 1, -1), q(0, 0);
 		d[x] = 0, q.push_back(x);
 		while (q.size()) {
@@ -25,31 +25,20 @@ int main() {
 		return d;
 	};
 
-	auto get_opt = [&](const auto& cmp) {
-		int res = 0;
-		for (int i = 1; i <= n; i++) if (cmp(res) < cmp(i)) res = i;
-		return res;
+	auto get = [&](const auto& f) {
+		int p = 0;
+		for (int i = 1; i <= n; i++) if (f(p) < f(i)) p = i;
+		return calc(p);
 	};
 
-	int x = get_opt([d1 = get_dist(1)](int i) { return d1[i]; });
-	auto dx = get_dist(x);
-
-	int y = get_opt([&](int i) { return dx[i]; });
-	auto dy = get_dist(y);
-
-	int z = get_opt([&](int i) { return dx[i] + dy[i]; });
-	auto dz = get_dist(z);
-
-	auto query = [&](int a, int b) -> int {
-		int rx = dx[a] + dx[b];
-		int ry = dy[a] + dy[b];
-		int rz = dz[a] + dz[b];
-		return max({ rx, ry, rz });
-	};
+	auto dx = get([d1 = calc(1)](int i) { return d1[i]; });
+	auto dy = get([&](int i) { return dx[i]; });
+	auto dz = get([&](int i) { return dx[i] + dy[i]; });
 
 	int q; cin >> q;
 	while (q--) {
 		int a, b; cin >> a >> b;
-		cout << query(a, b) << '\n';
+		int res = max({ dx[a] + dx[b], dy[a] + dy[b], dz[a] + dz[b] });
+		cout << res << '\n';
 	}
 }

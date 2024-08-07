1ec2ce63f2f53542a71511177d3254bd8e63b7bd
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2024-08-07 13:21:45 +0900

    .

--- UCPC/ucpc24_m.cpp
+++ UCPC/ucpc24_m.cpp
@@ -2,19 +2,26 @@
 #define fastio cin.tie(0)->sync_with_stdio(0)
 using namespace std;
 
-using i64 = long long;
-
 int main() {
 	fastio;
 	int n, k; cin >> n >> k;
 	string s; cin >> s;
-	vector _c(n + 1 << 1, 0);
-	auto c = _c.begin() + n + 1;
-	for (int i = 0, x = 0; i < 2 * n; i++) c[x += s[i] & 1 ? 1 : -1]++;
-	i64 res = 0, l = -n, r = n;
-	while (r - l > k) {
-		if (r == 0 || l && c[l] < c[r]) c[l + 2] += c[l], res += c[l++];
-		else c[r - 2] += c[r], res += c[r--];
+
+	vector c1(n + 1, 0), c2(n + 1, 0);
+	for (int i = 0, x = 0; i < 2 * n; i++) {
+		x += s[i] & 1 ? 1 : -1;
+		if (x > 0) c1[x]++;
+		if (x < 0) c2[-x]++;
 	}
+
+	vector a(n + 1, 0LL), b(n + 1, 0LL);
+	for (int i = n - 1; i >= 0; i--) {
+		a[i] = a[i + 1] + c1[i + 1];
+		b[i] = b[i + 1] + c2[i + 1];
+		if (i > 1) c1[i - 1] += c1[i + 1], c2[i - 1] += c2[i + 1];
+	}
+
+	auto res = 1LL << 60;
+	for (int i = 0; i <= k; i++) res = min(res, a[i] + b[k - i]);
 	cout << res << '\n';
 }

451eac7fe2f23737fcbb4c12e3d6e6a147f7c8d4
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2024-08-18 18:06:30 +0900

    .

---
+++ ICPC/RUSSIA/nwrrc19_e.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+
+int main() {
+	fastio;
+	int n, m; cin >> n >> m;
+	vector adj(n + 1, vector(0, 0));
+	for (int i = 1; i < n; i++) {
+		int a, b; cin >> a >> b;
+		adj[a].push_back(b);
+		adj[b].push_back(a);
+	}
+
+	vector q(m, 0), d(n + 1, -1), dp(n + 1, 0);
+	for (int i = 0; i < m; i++) {
+		cin >> q[i];
+		d[q[i]] = 0;
+		dp[q[i]] = 1;
+	}
+	for (int i = 0; q.size(); i++) {
+		vector nq(0, 0);
+		for (int x : q) {
+			for (int nxt : adj[x]) {
+				if (d[nxt] == -1 || d[nxt] == i + 1) dp[nxt] += dp[x];
+				if (d[nxt] == -1) d[nxt] = i + 1, nq.push_back(nxt);
+			}
+		}
+		q.swap(nq);
+	}
+
+	int p = -1;
+	for (int i = 1; i <= n; i++) if (dp[i] == m) p = i;
+	if (p == -1) cout << "NO" << '\n';
+	else cout << "YES" << '\n' << p << '\n';
+}

9bf43804cea085489938687fb0cb8d74c090e62b
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2024-09-04 00:57:13 +0900

    .

--- ICPC/HONGKONG/hongkong23_c.cpp
+++ ICPC/HONGKONG/hongkong23_c.cpp
@@ -2,7 +2,7 @@
 #define fastio cin.tie(0)->sync_with_stdio(0)
 using namespace std;
 
-auto transpose = [](const auto& v) {
+auto transpose = [](auto v) {
 	const int n = v.size();
 	const int m = v[0].size();
 	vector res(m, string(n, 0));
@@ -14,61 +14,50 @@ auto transpose = [](const auto& v) {
 };
 
 auto construct = [](int n, int m) {
-	if (n == 2 && m == 3) {
+	if (n == 3 && m == 2) {
 		vector res(n, string(m, '0'));
-		res[0] = "001";
-		res[1] = "011";
+		res = { "00", "01", "11" };
 		return res;
 	}
-
 	vector res(n, string(m, '0'));
-	int sz = 1, p = 0;
-	while (1 << sz < n) sz++;
+	int p = 0, sz = m - 1 ? __lg(m - 1) + 1 : 1;
 	for (int x = 0; x < sz; x++) {
-		for (int i = 0; i < n; i++) {
-			if (i >> x & 1) res[i][p]++;
-			else res[i][p + 1]++;
+		for (int i = 0; i < m; i++) {
+			if (i >> x & 1) res[p][i]++;
+			else res[p + 1][i]++;
 		}
 		p += 2;
 	}
-
 	auto check = [&](const string& s) -> bool {
-		for (int j = 0; j < 2 * sz; j++) {
-			string t(n, '0');
-			for (int i = 0; i < n; i++) t[i] = res[i][j];
-			if (s == t) return 0;
-		}
+		for (int i = 0; i < 2 * sz; i++) if (res[i] == s) return 0;
 		return 1;
 	};
-
-	for (int x = 0; (n >= 30 || x < 1 << n - 1) && p + 1 < m; x++) {
-		if (2 * __builtin_popcount(x) == n) continue;
-		string s(n, '0');
-		for (int i = 0; i < min(n, 30); i++) if (x >> i & 1) s[i]++;
+	for (int x = 0; (m >= 30 || x < 1 << m - 1) && p + 1 < n; x++) {
+		if (2 * __builtin_popcount(x) == m) continue;
+		string s(m, '0');
+		for (int i = 0; i < m; i++) if (x >> i & 1) s[i]++;
 		if (!check(s)) continue;
-		for (int i = 0; i < n; i++) res[i][p] = s[i];
-		for (int i = 0; i < n; i++) res[i][p + 1] = s[i] ^ 1;
-		p += 2;
+		res[p++] = s;
+		for (char& c : s) c ^= 1;
+		res[p++] = s;
 	}
-
-	vector brute(n, 0);
-	for (int i = n / 2; i < n; i++) brute[i] = 1;
-	while (p < m) {
-		string s(n, '0');
-		for (int i = 0; i < n; i++) if (brute[i]) s[i]++;
-		next_permutation(brute.begin(), brute.end());
+	vector b(m, 0);
+	for (int i = m / 2; i < m; i++) b[i] = 1;
+	while (p < n) {
+		string s(m, '0');
+		for (int i = 0; i < m; i++) if (b[i]) s[i]++;
+		next_permutation(b.begin(), b.end());
 		if (!check(s)) continue;
-		for (int i = 0; i < n; i++) res[i][p] = s[i];
-		p++;
+		res[p++] = s;
 	}
 	return res;
 };
 
 auto sol = [](int n, int m) {
 	if (n * m & 1) return vector(0, string{});
-	bool f = n > m;
+	bool f = n < m;
 	if (f) swap(n, m);
-	if (n < 30 && 1 << n < m) return vector(0, string{});
+	if (m < 30 && 1 << m < n) return vector(0, string{});
 	auto res = construct(n, m);
 	if (f) res = transpose(res);
 	return res;

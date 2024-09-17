4eb7b4354109f16fbab7698b78578de3e78c8047
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2024-09-18 01:31:24 +0900

    .

--- UCPC/ucpc24_h.cpp
+++ UCPC/ucpc24_h.cpp
@@ -2,14 +2,21 @@
 #define fastio cin.tie(0)->sync_with_stdio(0)
 using namespace std;
 
+auto ccw = [](const auto& a, const auto& b, const auto& c) {
+	int x1 = b.first - a.first;
+	int y1 = b.second - a.second;
+	int x2 = c.first - b.first;
+	int y2 = c.second - b.second;
+	return 1LL * x1 * y2 - 1LL * x2 * y1;
+};
+
 int main() {
 	fastio;
 	int n, q; cin >> n >> q;
-	vector v(n, pair(0, 0));
+	vector v(n, pair(0, 0)), qu(q, pair(0, 0));
 	for (auto& [a, b] : v) cin >> a >> b;
-	vector qu(q, pair(0, 0));
 	for (auto& [a, b] : qu) cin >> a >> b;
-
+	
 	int x = 0;
 	auto f = [](auto p) { return p.first > 0 || p.first == 0 && p.second > 0; };
 	while (f(v[x])) x++;
@@ -17,29 +24,26 @@ int main() {
 	rotate(v.begin(), v.begin() + x, v.end()); x = 0;
 	while (f(v[x])) x++;
 
-	auto ccw = [](const auto& a, const auto& b, const auto& c) {
-		int x1 = b.first - a.first;
-		int y1 = b.second - a.second;
-		int x2 = c.first - b.first;
-		int y2 = c.second - b.second;
-		return 1LL * x1 * y2 - 1LL * x2 * y1;
-	};
-
 	auto is_in = [&](const auto& p) -> bool {
 		if (p == pair(0, 0)) return 1;
-		if (ccw(pair(0, 0), v[x - 1], p) >= 0 &&
-			ccw(pair(0, 0), v[x], p) <= 0) {
+		if (ccw(pair(0, 0), v[x - 1], p) > 0 &&
+			ccw(pair(0, 0), v[x], p) < 0) {
 			return ccw(v[x - 1], v[x], p) >= 0;
 		}
-		if (ccw(pair(0, 0), v[n - 1], p) >= 0 &&
-			ccw(pair(0, 0), v[0], p) <= 0) {
+		if (ccw(pair(0, 0), v[n - 1], p) > 0 &&
+			ccw(pair(0, 0), v[0], p) < 0) {
 			return ccw(v[n - 1], v[0], p) >= 0;
 		}
-		auto [lo, hi] = p.first > 0 ? pair(0, x - 1) : pair(x - 1, n - 1);
+		auto [lo, hi] = f(p) ? pair(0, x) : pair(x, n);
 		while (lo + 1 < hi) {
 			int mid = lo + hi >> 1;
 			if (ccw(pair(0, 0), v[mid], p) >= 0) lo = mid;
 			else hi = mid;
+		}
+		if (lo && ccw(v[lo - 1], v[lo], p) == 0) {
+			auto a = v[lo - 1], b = v[lo];
+			if (a > b) swap(a, b);
+			return p <= b;
 		}
 		return ccw(v[lo], v[hi], p) >= 0;
 	};

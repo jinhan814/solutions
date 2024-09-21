394e1ec7d3a24e4564891cb1b832b9b1eaf60c1c
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2024-09-21 14:27:19 +0900

    .

--- ICPC/NWERC/nwerc19_g.cpp
+++ ICPC/NWERC/nwerc19_g.cpp
@@ -7,11 +7,8 @@ int main() {
 	int n, k; cin >> n >> k;
 	vector v(n, 0.);
 	for (int i = 0; i < n; i++) cin >> v[i], v[i] /= 100;
-
 	auto calc = [&](int i) {
-		if (k == 1) return 1. * k / n;
-		double res = v[i] * k / n;
-		double p = 1. * k / n;
+		double res = v[i] * k / n, p = 1. * k / n;
 		int a = n - k, b = n - 1;
 		while (a) {
 			i = i ? i - 1 : n - 1;
@@ -20,7 +17,6 @@ int main() {
 		}
 		return res;
 	};
-
 	cout << fixed << setprecision(10);
 	for (int i = 0; i < n; i++) cout << 100 * calc(i) << ' ';
 	cout << '\n';

14f43a9deef7ff1bcb0024a9603b7cb6dbcbf706
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2024-10-04 14:24:36 +0900

    .

---
+++ ICPC/UKIEPC/ukiepc19_b.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+
+auto f = [](string a, string b) {
+	return abs(stoll(a) - stoll(b));
+};
+
+int main() {
+	fastio;
+	string s; cin >> s;
+	string a = s[0] + string(s.size() - 1, 48);
+	string b = s[0] + string(s.size() - 1, 48);
+	if (++b[0] == 58) b = "10" + b.substr(1);
+	cout << (f(a, s) < f(b, s) ? a : b) << '\n';
+}
---
+++ ICPC/UKIEPC/ukiepc19_f.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+
+int main() {
+	fastio;
+	int n, k; cin >> n >> k;
+	vector v(n, 0);
+	for (int i = 0; i < n; i++) cin >> v[i];
+	sort(v.begin(), v.end());
+	int res = 0;
+	for (int l = 0, r = n - 1; l <= r; res++) {
+		if (l < r && v[l] + v[r] <= k) l++;
+		r--;
+	}
+	cout << res << '\n';
+}

f552869570760056e6eaabbc642050e6da453edd
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2024-10-07 18:52:53 +0900

    .

--- README.md
+++ README.md
@@ -79,7 +79,7 @@
 -- ucpc24
 ```
 
-### UNIVERSITY
+### UNIV
 
 ```
 # KAIST
---
+++ UNIV/KCPC/kcpc24_2b.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+
+using i64 = long long;
+
+int main() {
+	fastio;
+	i64 n, m; cin >> n >> m;
+	i64 res = 0, prv = 1LL << 60;
+	for (int i = 0; i < n; i++) {
+		i64 x; cin >> x;
+		if (abs(x - prv) < m) x = 1LL << 60;
+		if (x >> 60) res++; prv = x;
+	}
+	cout << res << '\n';
+}

86d6d033b640a279f759be0a664777a995b4af2a
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2025-08-23 19:42:33 +0900

    .

---
+++ SCPC/pre24_1_b.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+auto sol = [](int n, auto v) {
+	sort(v.begin(), v.end());
+	int ret = 0;
+	for (int i = 0; i < n / 4; i++) {
+		int val = v[n - 1 - i] - v[n / 4 - 1 - i];
+		ret += 2 * val;
+	}
+	return ret;
+};
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	int tc; cin >> tc;
+	for (int tc_num = 1; tc_num <= tc; tc_num++) {
+		int n; cin >> n;
+		vector v(n, 0);
+		for (int i = 0; i < n; i++) cin >> v[i];
+		cout << "Case #" << tc_num << '\n';
+		cout << sol(n, v) << '\n';
+	}
+}
